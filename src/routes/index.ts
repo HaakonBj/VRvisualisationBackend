6a63db0af65f46d15d27ef0c24c99954912d8418
Håkon Bjørklund <hkonjork@gmail.com>
2017-02-06 17:27:17 +0100

    Added clone git repo functionality

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-
-
+var git = require('nodegit');
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
@@ -12,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 // git repo url handler
-//TODO: sanitize input
+//TODO: sanitize input? Is it required when using clone? 
 router.post('/', function (req, res) {
   console.log(req.body.gitUrl);
     res.render('index',{
@@ -21,10 +20,25 @@ router.post('/', function (req, res) {
       success: "success, repo url recieved:",
       repo: req.body.gitUrl
     });
+
+  var localPath = require("path").join(__dirname, "../localRepo");
+   
+   
+  var repo = git.Clone.clone(req.body.gitUrl, localPath);
+
+    //TODO clone data, output cloning
+    //open, retrieve the data
+    //console log out the data, to see what information is available.
+  var errorAndAttemptOpen = function() {
+    return git.Repository.open(localPath);
+  };
   
-    //TODO retrieve data from git using this url
-    
+  repo.catch(errorAndAttemptOpen)
+  .then(function(repository) {
+    // Access any repository methods here.
+    console.log("Is the repository bare? %s", Boolean(repository.isBare()));
   });
 
+});
 
 module.exports = router;
